33a12a3c7f399bd2b2d6a810cafe5545000dfeaf
Andrés Sánchez <andres.sanchez@epfl.ch>
2020-06-05 21:24:15 +0200

    i3 creating new workspaces

---
+++ .config/i3/empty_workspace.sh
@@ -0,0 +1,6 @@
+if [[ $1 == "move" ]]; then
+    newContainer=$(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+    i3-msg move container to workspace $newContainer && i3 workspace $newContainer
+else
+    i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+fi
