de6224262a472b69d016a6bb19c5d29d7f6cc888
Andrés Sánchez <andres.sanchez@epfl.ch>
2020-06-05 21:26:58 +0200

    script new workspaces sharing variables

--- .config/i3/empty_workspace.sh
+++ .config/i3/empty_workspace.sh
@@ -1,6 +1,6 @@
+new_workspace=$(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
 if [[ $1 == "move" ]]; then
-    newContainer=$(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
-    i3-msg move container to workspace $newContainer && i3 workspace $newContainer
+    i3-msg move container to workspace $new_workspace && i3 workspace $new_workspace
 else
-    i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+    i3-msg workspace $new_workspace
 fi
